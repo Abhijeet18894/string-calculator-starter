4957675a986e92f7808172b10bc92eac34fc70a2
Abhijeet <abhijeet@18.com>
2021-10-16 17:38:19 +0530

    tested two Numbers New Line Delimited Return Sum

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -24,4 +24,7 @@ class StringCalculatorShould {
    public void twoNumbersCommaDelimitedReturnSum()throws Exception {
    	assertEquals(stringCalculator.add("1,2"),3);
    }
+   public void twoNumbersNewLineDelimitedReturnSum()throws Exception {
+   	assertEquals(stringCalculator.add("1\n2"),3);
+   }
 }
